664c3bb9c2f69e70c63d8dd3942b8d2906345973
Marcelo767n <luismarcelo767n@gmail.com>
2024-09-04 19:27:35 -0400

    clase 04.09

--- codigos/ejercicios de python/notas.py
+++ codigos/ejercicios de python/notas.py
@@ -1,9 +1,16 @@
-materias=int(input('ingrese el numero de calificaciones '))
-notas=[0]
-for i in range(materias):
-    valor=float(input(f"Ingrese el valor de la nota {i+1}: "))
-    notas.append(valor)
-    
-promedio=sum(notas)/materias
-print(f"el promedio es: {promedio}")
+num_estudiantes = int(input("Ingrese el número de estudiantes: "))
+num_notas = int(input("Ingrese el número de notas por estudiante: "))
 
+matrizdenotas=[]
+for i in range(num_estudiantes):
+    print(f'ingrese el numero de notas del estudiante  {i+1} ')
+    notas=[]
+    for j in range(num_notas):
+        nota=float(input(f'ingrese la nota {j+1} '))
+        notas.append(nota)
+    matrizdenotas.append(notas)
+print(matrizdenotas)
+
+for k in range (num_estudiantes):
+    promedio=sum(matrizdenotas[k])/num_notas
+    print(f'estudiante {k+1}, promedio: {promedio} ')
